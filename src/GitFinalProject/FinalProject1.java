81dc8b85bdc47586782b5189adcd7b762e899afd
muhammedlyas <muhammedilys@gmail.com>
2022-09-29 03:48:40 +0300

    2nd commit

---
+++ src/GitFinalProject/FinalProject1.java
@@ -0,0 +1,12 @@
+package GitFinalProject;
+
+public class FinalProject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is the code for the audii");
+
+	}
+
+}
